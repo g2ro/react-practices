f163034319bd0c6876559b444bf0701e95ad92a9
g2ro <kilho603@gmail.com>
2025-02-10 15:57:13 +0900

    Component: Property #1

---
+++ 03.Component - Property/ex01/src/GroceryItem.js
@@ -0,0 +1,28 @@
+// 방법 1
+// import React from 'react';
+
+// function GroceryItem(props) {
+
+//     return (
+//         <li>
+//             <strong>{props.name}</strong>
+//             <span>{props.count}</span>
+//         </li>
+//     );
+// }
+
+// export default GroceryItem;
+
+// 방법 2
+import React from 'react';
+
+function GroceryItem({name, count}) {
+    return (
+        <li>
+            <strong>{name}</strong>
+            <span>{count}</span>
+        </li>
+    );
+}
+
+export default GroceryItem;
