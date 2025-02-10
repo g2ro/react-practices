18ffd121703b54e06d62bcf5b29560c4ba6b2c2f
g2ro <kilho603@gmail.com>
2025-02-10 15:54:37 +0900

    Pure React(React API) 컴포넌트 - (예제: ex04)

---
+++ 02.JSX Tutorials/ex04/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './header';
+import Contents from './Contents';
+
+// React.createElement('div', null, Header, React.createElement(...), ...);
+// 여기서 Header가 호출된다면 => html 엘리먼트가 아닌 것을 파악하고 -> react에서 자동으로 호출한다.
+// component react api 생성  
+function App() {
+    /*
+    return (
+        <div id="App">
+            <Header />
+            <Contents/>
+        </div>
+    );
+    */
+
+    return React.createElement('div', {id: 'App'}
+        , React.createElement(Header, null), React.createElement(Contents, null))
+}
+
+export {App};
---
+++ 02.JSX Tutorials/ex04/src/Contents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contents(props) {
+    // return (
+    //     <div>
+    //         <p>함수 컴포넌트</p>
+    //     </div>
+
+        
+    // );
+    return React.createElement('p', null, '함수 컴포넌트');
+}
+
+export default Contents;
