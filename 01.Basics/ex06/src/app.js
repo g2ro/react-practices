76630f3007b1d9664282ec42737af71ebbcff0da
g2ro <kilho603@gmail.com>
2025-02-10 15:52:41 +0900

    06. React JSX 기반의 Application - (예제: ex06)

---
+++ 01.Basics/ex06/src/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App(){
+    // const App = document.createElement('div');
+    // App.textContent = "Hello World";
+
+    // const App = React.createElement('div', null, 'Hello World 10', parseInt('20'));
+
+    // return App;
+    return (
+        <div>
+            {'Hello World!!!!'}
+            {10}
+            {parseInt('20')}
+            <p>
+                {'test'}
+                <span>
+                    {'test2'}
+                </span>
+            </p>
+        </div>
+    )
+}
+
+export {App};
