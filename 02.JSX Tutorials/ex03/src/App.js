92e273dba36d59d1186df018c206fea799b98739
g2ro <kilho603@gmail.com>
2025-02-10 15:54:12 +0900

    함수 컴포넌트 - (예제: ex03)

--- 02.JSX Tutorials/ex03/src/App.js
+++ 02.JSX Tutorials/ex03/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './header';
+import Contents from './Contents';
+
+// React.createElement('div', null, Header, React.createElement(...), ...);
+// 여기서 Header가 호출된다면 => html 엘리먼트가 아닌 것을 파악하고 -> react에서 자동으로 호출한다.
 
 function App() {
     return (
         <div>
-            <h1>Ex03</h1>
-            <p>함수 컴포넌트</p>
+            <Header />
+            <Contents/>
         </div>
     );
 }
---
+++ 02.JSX Tutorials/ex03/src/Contents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contents(props) {
+    return (
+        <div>
+            <p>함수 컴포넌트</p>
+        </div>
+    );
+}
+
+export default Contents;
