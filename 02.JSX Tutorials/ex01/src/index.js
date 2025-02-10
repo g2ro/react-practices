b612f4ab0b0cdb9263f91fabaecdb904cdc4bfcc
g2ro <kilho603@gmail.com>
2025-02-10 15:53:31 +0900

    특징 I: HTML과 비교 - (예제: ex01)

--- 02.JSX Tutorials/ex01/src/index.js
+++ 02.JSX Tutorials/ex01/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App.js';
 
+// console.log(process.env.NODE_ENV); //=> webpack에 의해 mode가 변경됨에 따라 결과값도 달라짐
+if(process.env.NODE_ENV === 'production'){
+  console.log = () => {};
+  console.info = () => {};
+  console.error = () => {};
+  console.warn = () => {};
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
