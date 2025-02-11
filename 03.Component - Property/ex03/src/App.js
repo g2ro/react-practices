605c3c85819b57327b30d8de861352374776627c
g2ro <kilho603@gmail.com>
2025-02-11 22:15:26 +0900

    Property

---
+++ 03.Component - Property/ex03/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './assets/css/styles.css';
+import GroceryList from './GroceryList';
+
+function App(props) {
+  const groceries = [
+    { name: 'milk', count: 10 },
+    { name: 'egg', count: 20 },
+    { name: 'bread', count: 5 },
+  ];
+
+  return (
+    <div id={'App'}>
+      <h1>{'Grocery List'}</h1>
+      <GroceryList groceries={groceries} />
+    </div>
+  );
+}
+
+export default App;
