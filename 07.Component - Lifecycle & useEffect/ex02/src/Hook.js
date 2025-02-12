86469d428284565c64fd342a02f1d592fa3175c9
g2ro <kilho603@gmail.com>
2025-02-13 00:33:20 +0900

    Lifecycle $ useEffect

--- 07.Component - Lifecycle & useEffect/ex02/src/Hook.js
+++ 07.Component - Lifecycle & useEffect/ex02/src/Hook.js
@@ -1,44 +1,59 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Hook({ color }) {
-    const [boxColor, setBoxColor] = useState(null);
-    const [title, setTitle] = useState('');
+  const [boxColor, setBoxColor] = useState(null);
+  const [title, setTitle] = useState('');
 
-    /**
-     *   [1] Alternative 01: getDerivedStateFromProps
-     */
+  /**
+   *   [1] Alternative 01: getDerivedStateFromProps
+   */
+  if (boxColor !== color) {
+    setBoxColor(color);
+  }
 
+  /**
+   *   [2-1] After Rendering 함수 - Alternative 02: componentDidUpdate
+   */
+  useEffect(() => {
+    console.log('After Rendering1');
+  });
 
-    /**
-     *   [2-1] After Rendering 함수 - Alternative 02: componentDidUpdate
-     */
-    /**
-     *  [2-2] After Rendering 함수 - 어떤 특정 상태(boxColor)의 변화에 반응(관심 분리)
-     */
-    
-    
+  /**
+   *  [2-2] After Rendering 함수 - 어떤 특정 상태(boxColor)의 변화에 반응(관심 분리)
+   */
+  useEffect(() => {
+    console.log('After Rendering2: boxColor Updated');
+  }, [boxColor]);
 
-    /**
-     *  [3] Alternative 03: componentDidMount & componentWillUnmount
-     */
+  useEffect(() => {
+    console.log('After Rendering2: title Updated');
+  }, [title]);
+  /**
+   *  [3] Alternative 03: componentDidMount & componentWillUnmount
+   */
+  useEffect(() => {
+    console.log('After Mount(componentDidMount)');
 
+    return () => {
+      console.log('After Unmount(componentWillUnmount)');
+    };
+  }, []);
 
-
-
-
-    // render
-    return (
-        <>
-            <h3
-                style={ {
-                    width: 100,
-                    height: 50,
-                    backgroundColor: boxColor
-                } } />
-            <input
-                type='text'
-                value={ title }
-                onChange={ (e) => setTitle(e.target.value) } />
-        </>
-    );
+  // render
+  return (
+    <>
+      <h3
+        style={{
+          width: 100,
+          height: 50,
+          backgroundColor: boxColor,
+        }}
+      />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+    </>
+  );
 }
--- 07.Component - Lifecycle & useEffect/ex03/src/App.js
+++ 07.Component - Lifecycle & useEffect/ex03/src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
-import './assets/scss/App.scss'
+import './assets/scss/App.scss';
 import Clock from './Clock';
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = this.__getCurrentTime();
+  }
 
-    
-    render() {
-        return (
-                <Clock
-                    title={`ex03: Clock Component I:`}
-                    hours={'00'}
-                    minutes={'00'}
-                    seconds={'00'} />
-        )
-    
-    }
+  __getCurrentTime() {
+    const now = new Date();
+
+    return {
+      hours: now.getHours(),
+      minutes: now.getMinutes(),
+      seconds: now.getSeconds(),
+      ticks: this.state ? this.state.ticks + 1 : 1,
+    };
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState(this.__getCurrentTime());
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    console.log('App.componentWillUnmount');
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return this.state.ticks % 10 === 0 ? null : (
+      <Clock
+        title={`ex03: Clock Component I:${this.state.ticks}`}
+        hours={this.state.hours}
+        minutes={this.state.minutes}
+        seconds={this.state.seconds}
+      />
+    );
+  }
 }
