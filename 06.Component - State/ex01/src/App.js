15f29376df0310b72ea52ffa49901fc48dbe5445
g2ro <kilho603@gmail.com>
2025-02-12 23:53:43 +0900

    State

--- 06.Component - State/ex01/src/App.js
+++ 06.Component - State/ex01/src/App.js
@@ -3,14 +3,14 @@ import Incrementor01 from './Incrementor01';
 import Incrementor02 from './Incrementor02';
 
 function App() {
-    return (
-        <>
-            <h2>ex04 - Component State?</h2>
-            <Incrementor01 />
-            <br/>
-            <Incrementor02 />
-        </>
-    );
+  return (
+    <>
+      <h2>ex04 - Component State?</h2>
+      <Incrementor01 val={0} step={10} />
+      <br />
+      <Incrementor02 val={0} step={10} />
+    </>
+  );
 }
 
 export default App;
--- 06.Component - State/ex01/src/Incrementor01.js
+++ 06.Component - State/ex01/src/Incrementor01.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 
 export default class extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.val,
+      count2: 10, // 데이터가 여러개 있을때
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                <button>{'+'}</button>
-                {' '}
-                { 0 }
-                {' '}
-                <button>{'-'}</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + this.props.step,
+              // count2: this.state.count2 // 바뀔 data만 바꾸면 자동으로 render진행해준다.
+            });
+            // anti parttern
+            // this.state.count += 1;
+            // this.forceUpdate();
+          }}
+        >
+          {'+'}
+        </button>{' '}
+        {this.state.count}{' '}
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count - this.props.step,
+            });
+          }}
+        >
+          {'-'}
+        </button>
+      </div>
+    );
+  }
 }
