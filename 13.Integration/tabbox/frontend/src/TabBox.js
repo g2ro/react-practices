93355e640971377f382ff0dd651fc8991590a5ee
g2ro <kilho603@gmail.com>
2025-02-13 13:04:09 +0900

    bottom to top

--- 13.Integration/tabbox/frontend/src/TabBox.js
+++ 13.Integration/tabbox/frontend/src/TabBox.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tabs from './Tabs';
 import TabView from './TabView';
 import { Tab_Box } from './assets/scss/TabBox.scss';
 import data from './assets/json/data';
 
 function TabBox(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const selectTab = (no) => {
+    console.log(`${no} selected`);
+    setActiveIndex(data.findIndex((e) => e.no === no));
+  };
+
   return (
     <div className={Tab_Box}>
-      <Tabs tabs={data} />
-      <TabView />
+      <Tabs
+        tabs={data.map((e, index) => {
+          // const n = Object.assign({}, e);
+          // const n = {
+          //   no : e.no,
+          //   name: e.name,
+          //   etc,,,
+          // } => assign의 기능 -> 새로운 객체이지만, 내부 요소는 그대로
+          // const {...rest } = e; // Object.assign과 같은 기능
+          const { contents, ...rest } = e; // contents와 rest로 e 객체를 분할 한다.
+          if (index === activeIndex) {
+            rest.active = true;
+          }
+          return rest;
+        })}
+        selectTab={selectTab}
+      />
+      <TabView contents={data[activeIndex].contents} />
     </div>
   );
 }
--- 13.Integration/tabbox/frontend/src/Tabs.js
+++ 13.Integration/tabbox/frontend/src/Tabs.js
@@ -6,11 +6,17 @@ const StyledUL = styled.ul`
   height: 24px;
 `;
 
-function Tabs({ tabs }) {
+function Tabs({ tabs, selectTab }) {
   return (
     <StyledUL>
       {tabs.map((tab) => (
-        <TabItem key={tab.no} name={tab.name} active={tab.active} />
+        <TabItem
+          key={tab.no}
+          no={tab.no}
+          name={tab.name}
+          active={tab.active}
+          selectTab={selectTab}
+        />
       ))}
     </StyledUL>
   );
