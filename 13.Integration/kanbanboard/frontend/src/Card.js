5baba858b1c157e2da20bc35a1cf976dc91a0027
g2ro <kilho603@gmail.com>
2025-02-14 17:10:27 +0900

    add click event TaskList

--- 13.Integration/kanbanboard/frontend/src/Card.js
+++ 13.Integration/kanbanboard/frontend/src/Card.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { _Card, Card_Title, Card_Title_Open } from './assets/scss/Card.scss';
 import TaskList from './TaskList';
 function Card({ data }) {
+  const [visual, setVisual] = useState(true);
   return (
     <div className={_Card}>
-      <div className={`${Card_Title} ${Card_Title_Open}`}>{data.title}</div>
+      <div
+        className={[Card_Title, visual ? Card_Title_Open : null].join(' ')}
+        onClick={() => {
+          setVisual(!visual);
+        }}
+      >
+        {data.title}
+      </div>
       <div className="Card_Details">
         {data.description}
-        <TaskList tasks={data.tasks} />
+        <TaskList tasks={visual ? data.tasks : null} />
       </div>
     </div>
   );
