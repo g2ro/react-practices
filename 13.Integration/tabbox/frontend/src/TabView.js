de83d670175bdc060f18b2d2a9af758f7064d152
g2ro <kilho603@gmail.com>
2025-02-17 14:10:07 +0900

    -test build

--- 13.Integration/tabbox/frontend/src/TabView.js
+++ 13.Integration/tabbox/frontend/src/TabView.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Tab_View } from './assets/scss/TabView.scss';
 
 function TabView({ contents }) {
-  return <div className={Tab_View}>{contents}</div>;
+  return <div className={Tab_View}>
+    <span>{'hello world!!'}</span>
+    <span>{contents}</span>
+    </div>;
 }
 
 export default TabView;
